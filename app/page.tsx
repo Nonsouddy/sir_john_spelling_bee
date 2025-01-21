76cb6465f0ea1be8e91552babf5cfbd7dcd8ef15
Patrick Harry <harrypatrick.dev@gmail.com>
2025-01-21 13:29:05 +0100

    errors were fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Dashboard from './(Admin)/dashboard/page'
 import HomePage from './components/HomePage'
-import FAQPage from './faqPage/Page'
+
 
 
 
@@ -12,10 +12,10 @@ import FAQPage from './faqPage/Page'
 function page() {
   return (
     <>
-{/* <HomePage/> */}
+<HomePage/>
  
   {/* <Dashboard/> */}
-  <FAQPage/>
+
 
 
 
