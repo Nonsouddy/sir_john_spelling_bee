01c4c429971d0919a427e92e1dd47b419c16dcf9
Patrick Harry <harrypatrick.dev@gmail.com>
2025-02-19 14:16:35 +0100

    mission section was adjusted

--- app/Type/MissionAndVission.tsx
+++ app/Type/MissionAndVission.tsx
@@ -9,7 +9,7 @@ function MissionAndVission() {
         <>
             <div className="min-h-screen w-full ">
                 <div className="mx-auto sm:px-0 md:overflow-hidden lg:overflow-hidden ">
-                    <div className="grid grid-cols-1 md:flex lg:flex lg:gap-2 mb-8 md:mt-0 gap-6 sm:mt-0 gap-0">
+                    <div className="grid grid-cols-1 md:flex lg:flex lg:gap-2 mb-8 md:mt-0 gap-6  gap-0">
                         <div className="w-full md:w-full lg:w-[695px] md:h-[500px] lg:h-[700px] lg:ml-0 lg:mt-[50px]  bg-heroBlue rounded-none md:rounded-r-xl lg:rounded-r-2xl">
                        
                             <div className="mx-auto my-auto flex-items-center justify-center lg:w-[450px] lg:h-[250px] md:w-[200px] h-[200px] sm:h-[200px] mt-[40px] w-[300px] bg-cover bg-center bg-no-repeat rounded-lg">
@@ -20,7 +20,7 @@ function MissionAndVission() {
 
 
 
-                            <div className="flex flex-col items-center gap-3 mx-auto md:mx-0 w-[90%] sm:w-[375px] md:w-[451px]  h-[150px] sm:h-[170px] mt-[50px] sm:text-center md:h-[187px]  md:ml-[20px] md:mt-[50px] p-0">
+                            <div className="flex flex-col items-center gap-3 mx-auto md:mx-0 w-[90%] sm:w-[375px] md:w-[451px]  h-[150px] sm:h-[170px] mt-[50px] mb-[100px] sm:text-center md:h-[187px]  md:ml-[20px] md:mt-[50px] p-0">
                                 < h2 className="w-[90%] sm:w-[350px] md:w-[451px] h-[50px] sm:h-[60px] md:h-[66px] font-['Comic_Sans_MS'] font-bold  text-3xl sm:text-4xl md:text-[48px] leading-[112%] text-center text-accentOrange mx-auto md:mx-0 flex-none order-0 self-stretch ">
                                     Vission </h2>
                                 <p className="w-[90%] sm:w-[350px]  md:w-[451px] h-auto min-h-[70px] sm:min-h-[80px] md:min-h-[92px] font-['Istok_Web'] font-normal  text-base sm:text-lg md:text-2xl leading-relaxed md:leading-[35px] text-center text-gray-900 mx-auto md:mx-0 flex-none order-1 self-stretch">Through words, we can raise children with tools to dream, build, create, break away boundaries and explore the world.</p>
@@ -29,8 +29,8 @@ function MissionAndVission() {
 
 
 
-                        <div className="w-full md:w-full lg:w-[695px] h-[600px] md:h-[500px] lg:h-[700px] right-0 md:ml-[50px] md:mt-[300px] lg:mt-[180px] sm:mt-[30px] bg-orange-50 rounded-none md:rounded-l-2xl">
-                            <div className="flex flex-col items-center gap-3  mx-auto md:mx-0 w-[90%] sm:w-[375px] md:w-full h-[150px] sm:h-[170px] mt-[50px]  md:h-[187px] md:ml-[50px] md:mt-[50px] p-0">
+                        <div className="w-full md:w-full lg:w-[695px] h-[600px] md:h-[500px] lg:h-[700px] right-0 md:ml-[50px] md:mt-[300px] lg:mt-[180px] sm:mt-[100px] bg-orange-50 rounded-none md:rounded-l-2xl">
+                            <div className="flex flex-col items-center gap-3  mx-auto md:mx-0 w-[90%] sm:w-[375px] md:w-full h-[150px] sm:h-[170px] mt-[50px] mb-[100px]   md:h-[187px] md:ml-[50px] md:mt-[50px] p-0">
 
                                 < h2 className="w-[90%] sm:w-[350px] md:w-[451px] h-[50px] sm:h-[60px] md:h-[66px] font-['Comic_Sans_MS'] font-bold text-3xl sm:text-4xl md:text-[48px] leading-[112%] text-center text-heroBlue mx-auto md:mx-0 flex-none order-0 self-stretch">
                                     Mission</h2>
