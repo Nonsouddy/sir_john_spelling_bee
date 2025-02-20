e983810933830a3642c95413d2c4ec8b421a4fe2
Priest <Charleschukwuemeka47@gmail.com>
2025-02-20 23:36:27 +0100

    Feat: Add the loading page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
         <div className="flex flex-col justify-center items-center min-h-dvh">
             <h2 className="mb-4 font-bold text-lg md:text-xl xl:text-2xl">404 - Page Not Found</h2>
             <p className="mb-4">Sorry, the page you are looking for doesn't exist.</p>
-            <button className="bg-primaryYellow hover:bg-inherit px-8 py-3 rounded-3xl text-textBlack hover:bg-yellow-600 duration-300">
+            <button className="bg-primaryYellow hover:bg-inherit hover:bg-yellow-500 px-8 py-3 rounded-3xl text-textBlack duration-300">
                 <Link href="/">Go back home</Link>
             </button>
         </div>
