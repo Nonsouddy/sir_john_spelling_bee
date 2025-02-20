0d47ffd8ca121fee5f9f0f6ec7a03e2187f16c80
Priest <Charleschukwuemeka47@gmail.com>
2025-02-20 23:32:36 +0100

    Feat: Add the Not found page

--- app/(Admin)/admin/contestants/[id]/page.tsx
+++ app/(Admin)/admin/contestants/[id]/page.tsx
@@ -11,7 +11,9 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
     const { success, contestant } = await getContestant(id);
 
     if (success && contestant !== null) {
-        return <UserDataDisplay contestant={contestant} />;
+        return (
+            <UserDataDisplay contestant={contestant} />
+        )
     }
 
     return <main className="place-items-center grid">Contestant not found</main>;
---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+export default function NotFound() {
+    return (
+        <div className="flex flex-col justify-center items-center min-h-dvh">
+            <h2 className="mb-4 font-bold text-lg md:text-xl xl:text-2xl">404 - Page Not Found</h2>
+            <p className="mb-4">Sorry, the page you are looking for doesn't exist.</p>
+            <button className="bg-primaryYellow hover:bg-inherit px-8 py-3 rounded-3xl text-textBlack hover:bg-yellow-600 duration-300">
+                <Link href="/">Go back home</Link>
+            </button>
+        </div>
+    )
+}
