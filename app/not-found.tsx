97d6aac95ccdef63f10a90bbc00fb77ba36d6a20
Priest <Charleschukwuemeka47@gmail.com>
2025-02-21 09:04:11 +0100

    Fix: Fix the bug in the Not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
     return (
         <div className="flex flex-col justify-center items-center min-h-dvh">
             <h2 className="mb-4 font-bold text-lg md:text-xl xl:text-2xl">404 - Page Not Found</h2>
-            <p className="mb-4">Sorry, the page you are looking for doesn't exist.</p>
+            <p className="mb-4">Sorry, the page you are looking for doesn&apos;t exist.</p>
             <button className="bg-primaryYellow hover:bg-inherit hover:bg-yellow-500 px-8 py-3 rounded-3xl text-textBlack duration-300">
                 <Link href="/">Go back home</Link>
             </button>
