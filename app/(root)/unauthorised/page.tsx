b1ffda72680e00d6ece811c63f10ed99477b2e4d
Priest <Charleschukwuemeka47@gmail.com>
2025-02-27 16:22:37 +0100

    Feat: Add the Unauthorised page

--- app/(root)/unauthorised/page.tsx
+++ app/(root)/unauthorised/page.tsx
@@ -13,7 +13,7 @@ export default function UnauthorizedPage() {
             <div className="text-center">
                 <Shield className="mx-auto mb-4 w-16 h-16 text-gray-500" />
                 <h1 className="mb-4 font-bold text-2xl md:text-3xl xl:text-4xl">Unauthorized Access</h1>
-                <p className="mb-8 text-gray-500 text-xl">Sorry, you don't have permission to access this page.</p>
+                <p className="mb-8 text-gray-500 text-xl">Sorry, you don&apos;t have permission to access this page.</p>
                 <div className="space-x-4">
                     <Button asChild>
                         <Link href="/">Go to Homepage</Link>
